0455e752db8c3fc171d391f015a77e6b5e551001
ksang <kaitoy@qq.com>
2016-09-28 21:24:31 +0800

    cross build for linux

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 . ./env.sh
-go build -o build/percentile percentile/*.go
+export GOOS=darwin
+export GOARCH=amd64
+go build -o build/darwin/amd64/percentile percentile/*.go
+
+export GOOS=linux
+export GOARCH=amd64
+go build -o build/linux/amd64/percentile percentile/*.go
